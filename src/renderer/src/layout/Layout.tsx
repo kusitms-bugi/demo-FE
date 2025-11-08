559b53e97800627567aef12c2ae4fcea11f37bfc
choihooo <hochoi8621@naver.com>
2025-11-08 12:57:51 +0900

    hotfix(dev): 개발 모드 네브바 삭제

--- src/renderer/src/layout/Layout.tsx
+++ src/renderer/src/layout/Layout.tsx
@@ -1,11 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import DevNavbar from '../components/DevNavbar/DevNavbar';
 import Header from './Header/Header';
 
 const Layout = () => {
   return (
     <div className="hbp:pt-[75px] min-h-screen pt-15">
-      <DevNavbar />
+      {/* <DevNavbar /> */}
       <Header />
       <Outlet />
     </div>
